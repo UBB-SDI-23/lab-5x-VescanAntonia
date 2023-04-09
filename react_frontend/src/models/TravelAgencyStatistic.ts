b7495276fddd4cd2ef652669e1fea2b85fb249b6
Vescan_Antonia <91742401+VescanAntonia@users.noreply.github.com>
2023-04-09 22:39:09 +0300

    Delete react_frontend directory

--- react_frontend/src/models/TravelAgencyStatistic.ts
+++
@@ -1,8 +0,0 @@
-export interface TravelAgencyStatistic {
-    id? : number;
-    name : string;
-    website : string;
-    address : string;
-    nrOfEmployees : number;
-    nrOfOffers : number;
-}
